2d3fdbb27c8527ac45fa2e241ba1389330f5d366
Jaihanuman64 <mohak64bansal@gmail.com>
2023-03-05 22:20:45 +0530

    creating generateFile function

---
+++ generateFile.js
@@ -0,0 +1,23 @@
+const fs= require('fs');
+const path = require('path');
+const {v4: uuid} = require('uuid'); //function v4 is renamed as uuid for further use inside the code
+
+const dirCodes = path.join(__dirname,"codes");
+
+if(!fs.existsSync(dirCodes)){
+    fs.mkdirSync(dirCodes, {recursive:true});
+}
+
+const generateFile = async(format,code) => {
+    const jobId = uuid();
+    const filename = `${jobId}.${format}`
+    const filepath = path.join(dirCodes, filename);
+    await fs.writeFileSync(filepath, content);
+    return filepath;
+
+
+};
+
+module.exports={
+    generateFile
+};
--- index.js
+++ index.js
@@ -2,6 +2,30 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    return res.json({hello: "world"});
+})
+
+app.post("/run",(req,res)=>{
+    // console.log(req.body);
+    
+
+    const {language = "cpp",code} = req.body; //sending cpp as default language //equivalent to const language = req.body.language; // const code = req.body.code;
+
+    if(code===undefined){
+        return res.status(400).json({success:false, error: "Empty code body"})
+    }
+
+    //need to generate a c++ file with content from the request
+    //we need to run the file and send the response
+
+    return res.json({language,code});
+})
+
+
 app.listen(5000, ()=>{
     console.log('listening on port 5000')
 })
